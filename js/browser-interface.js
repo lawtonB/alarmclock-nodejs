80f5dd78fd1cf156cc7f3dfa56a045850779a8a9
lawton browning and tim durward <student@epicodus.com>
2016-03-15 13:35:17 -0700

    complete alarm clock app

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,20 +3,31 @@ var moment = require('moment');
 
 $(document).ready(function() {
 
-  (function update_time(){
-      var now = moment().format('h:mm:ss');
-      $('#time').text(now);
-      setTimeout(update_time, 1000);
-  })();
+  // (function update_time(){
+  //     var now = moment().format('HH:mm:ss');
+  //     $('#time').text(now);
+  //     setTimeout(update_time, 1000);
+  // })();
 
-$("#time-button").click(function(){
-  var alarmTime = $("#input-time").val();
-  $('.time-body').text("You set the alarm for:" + alarmTime);
-
-  var $ins = $('#time, #input-time').on(function() {
-    if ($('#time').val() == $('#input-time')) {
-      alert("alarm activated");
-    }
-  });
+  $(document).ready(function update_time() {
+  $('#time').text(moment().format("HH:mm"));
+  setTimeout(update_time, 1000);
 });
+
+$(document).ready(function update_time_slow() {
+   setTimeout(update_time_slow, 1000);
+   if (alarmTime === consoleTime.text()) {
+     $(".time-body").append("<li>"+"WakeUp"+"</li>");
+   }
+ });
+
+  var consoleTime = $('#time').text(moment().format("HH:mm"));
+  var alarmTime = $("#input-time").val();
+
+
+  $("#time-button").click(function(){
+    alarmTime = $("#input-time").val();
+    $('.time-body').text("You set the alarm for:" + alarmTime);
+  });
+
 });
