69a0d47de68e48511dd8e38949abaf88a8fde489
lawton browning and tim durward <student@epicodus.com>
2016-03-15 10:36:50 -0700

    include basic user-interface of alarm

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,3 +1,7 @@
+var moment = require('moment');
 $(document).ready(function() {
-$("#time").text(moment().format("MMM Do YY"));
+  $("#time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+$("#time-button").click(function(){
+  $('.time-body').text("You set the alarm for:" + $("#input-time").val());
+});
 });
